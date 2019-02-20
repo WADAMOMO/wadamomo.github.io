eef8e1b2d77a61efabe453b26541827e6547de79
William Adamowicz <william.adamowicz@gmail.com>
2019-02-19 21:06:32 -0800

    re-add links in navbar

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log('hello from index');
+
+var confettiSettings = { target: 'my-canvas' };
+            var confetti = new window.ConfettiGenerator(confettiSettings);
+            confetti.render(); 
